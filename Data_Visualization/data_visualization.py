794e2079b9f78ccb8f055e3c07e92054aca5f363
Shefali Dhingra <71131216+Shefali-Dhingra@users.noreply.github.com>
2024-10-06 18:13:30 +0530

    Update data_visualization.py

--- Data_Visualization/data_visualization.py
+++ Data_Visualization/data_visualization.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt #to configure plots
 # Importing specific plots
 # from Visualization.visualization import Heatmap
 
-def Heatmap(data_obj):
-    """Heatmap
+# def Heatmap(data_obj):
+#     """Heatmap
 
-    :param data_obj: DataObject instance
-    :type data_obj: __main__.DataObject
-    """
-    fig = plt.figure(figsize=(16, 6))
-    sns.heatmap(data_obj.df.corr(), vmin=-1, vmax=1, annot=True, fmt='.2%').set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
-    return st.pyplot(fig)
+#     :param data_obj: DataObject instance
+#     :type data_obj: __main__.DataObject
+#     """
+#     fig = plt.figure(figsize=(16, 6))
+#     sns.heatmap(data_obj.df.corr(), vmin=-1, vmax=1, annot=True, fmt='.2%').set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
+#     return st.pyplot(fig)
     
 def main(data_obj):
     """Data Visualization main
@@ -22,28 +22,12 @@ def main(data_obj):
     :type data_obj: __main__.DataObject
     """
     st.header("DATA VISUALIZATION")
-    col1, col2 = st.columns(2)
-    col3, col4 = st.columns(2)
-    
-    with col1:
-        st.subheader("Correlation Heatmap")
-        Heatmap(data_obj)
-        
-    with col2:
-        st.subheader("Dataframe description")
-        st.dataframe(data_obj.df.describe())
-    
-    with col3:
-        st.subheader("Data types")
-        st.dataframe(data_obj.df.dtypes.astype(str))
-        
-    with col4:
-        st.subheader("Dataframe description")
-        st.dataframe(data_obj.df.describe())
 
-    # # Correlation matrix
-    # st.subheader("Correlation heatmap")
-    # Heatmap(data_obj)
+    # Correlation matrix
+    st.subheader("Correlation heatmap")
+    fig = plt.figure(figsize=(16, 6))
+    sns.heatmap(data_obj.df.corr(), vmin=-1, vmax=1, annot=True, fmt='.2%').set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
+    st.pyplot(fig)
 
 # Main
 if __name__ == "__main__":
