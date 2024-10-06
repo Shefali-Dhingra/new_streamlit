a2e83a36b2e82f150a33d9cdd047b6c946263239
Shefali Dhingra <71131216+Shefali-Dhingra@users.noreply.github.com>
2024-10-06 19:02:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,18 +39,7 @@ class Interface():
       dt_obj.df = pd.DataFrame.sample(dt_obj.data, n=3001, random_state=55043)
       dt_obj.filesize = dt_obj.df.size
       dt_obj.Non_Categorical_Variables=dt_obj.df.Quantity+dt_obj.df.Value+dt_obj.df.Weight
-      def show_heat_map(dt_obj.df):
-        st.title('Heat Map of Trade Data')
-        
-        # Compute correlation matrix
-        corr = self.dt_obj.df.corr()
-        
-        # Display the correlation matrix using Seaborn heatmap
-        fig, ax = plt.subplots(figsize=(10, 8))
-        sns.heatmap(corr, annot=True, fmt='.2f', cmap='coolwarm', ax=ax)
-        
-        # Show the plot in Streamlit
-        st.pyplot(fig)
+      
         # Side bar navigation menu with a select box
       menu = ['Welcome Page','Data Preview','Data Visualization']
       navigation = st.sidebar.selectbox(label="Select menu", options=menu)
@@ -62,7 +51,7 @@ class Interface():
 
       elif navigation == 'Data Visualization':
         with st.container():
-          show_heat_map(dt_obj.df)
+          Data_Visualization.data_visualization(dt_obj)
           
       else:
         Welcome_Page.welcome()
