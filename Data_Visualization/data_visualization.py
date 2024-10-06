62c0e0a8ec9c4db3519df1a8e0e1a44f84a8b3ee
Shefali Dhingra <71131216+Shefali-Dhingra@users.noreply.github.com>
2024-10-06 19:15:31 +0530

    Update data_visualization.py

--- Data_Visualization/data_visualization.py
+++ Data_Visualization/data_visualization.py
@@ -1,7 +1,7 @@
-import streamlit as st # Streamlit backend
-import seaborn as sns # For plotting
-import matplotlib.pyplot as plt # To configure plots
-import pandas as pd # To handle dataframes
+import streamlit as st 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+import pandas as pd 
 
 def Heatmap(non_cat):
     """Generates a heatmap of the correlation between numeric variables
@@ -9,7 +9,7 @@ def Heatmap(non_cat):
     :param non_cat: DataFrame with non-categorical columns (Quantity, Value, Weight)
     :type non_cat: pandas.DataFrame
     """
-    fig = plt.figure(figsize=(16, 12))
+    fig = plt.figure(figsize=(14, 10))
     sns.heatmap(non_cat.corr(), annot=True, fmt='.2f').set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
     st.pyplot(fig)
 
@@ -23,16 +23,13 @@ def BarGraph(data, import_export):
     """
     filtered_data = data[data['Import_Export'] == import_export]
     top_countries = filtered_data.groupby('Country')['Value'].agg(['min', 'max']).reset_index()
-
-    # Sorting by 'max' to get top 10 countries
     top_10_countries = top_countries.sort_values(by='max', ascending=False).head(10)
     
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(12, 8))
     top_10_countries.set_index('Country')[['min', 'max']].plot(kind='bar', ax=ax)
     ax.set_title(f'Top 10 Countries by {import_export} Value', fontsize=14)
     ax.set_xlabel('Country')
     ax.set_ylabel('Value')
-    
     st.pyplot(fig)
 
 def main(data_obj):
