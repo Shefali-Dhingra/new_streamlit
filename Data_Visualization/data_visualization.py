2f60a1f22109f1937f7867c249dc3e6b25c13506
Shefali Dhingra <71131216+Shefali-Dhingra@users.noreply.github.com>
2024-10-06 18:31:40 +0530

    Update data_visualization.py

--- Data_Visualization/data_visualization.py
+++ Data_Visualization/data_visualization.py
@@ -1,21 +1,6 @@
 # General import section
 import streamlit as st #streamlit backend
-import seaborn as sns #for plotting
-import matplotlib.pyplot as plt #to configure plots
-
-def Heatmap(data_obj):
-    """Heatmap
-
-    :param data_obj: DataObject instance
-    :type data_obj: __main__.DataObject
-    """
-    fig = plt.figure(figsize=(16, 6))
-    sns.heatmap(data_obj.df.corr(), vmin=-1, vmax=1, annot=True, fmt='.2%').set_title('Correlation Heatmap', fontdict={'fontsize':12}, pad=12)
-    return st.pyplot(fig)
-    
-    # height=300
-    return heatmap
-    
+from visualization import Heatmap
 def main(data_obj):
     """Data Visualization main
 
@@ -23,10 +8,25 @@ def main(data_obj):
     :type data_obj: __main__.DataObject
     """
     st.header("DATA VISUALIZATION")
-
-    # Correlation matrix
-    st.subheader("Correlation heatmap")
-    Heatmap(data_obj)
+    col1, col2 = st.columns(2)
+    col3, col4 = st.columns(2)
+    
+    with col1:
+        st.subheader("Dataframe Head and Shape")
+        st.dataframe(data_obj.df.head(10))
+        st.write(data_obj.df.shape)
+        
+    with col2:
+        st.subheader("Dataframe description")
+        st.dataframe(data_obj.df.describe())
+    
+    with col3:
+        st.subheader("Data types")
+        st.dataframe(data_obj.df.dtypes.astype(str))
+        
+    with col4:
+        st.subheader("Correlation heatmap")
+        Heatmap(data_obj)
 
 # Main
 if __name__ == "__main__":
